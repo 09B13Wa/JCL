e5779646522f24ed1684cdae03e640995bfc503a
jascha.merle <jascha.merle@epita.fr>
2021-04-15 22:47:38 +0200

    Set up

---
+++ Java/src/Architecture/UnsignedBinaryNumber.java
@@ -0,0 +1,101 @@
+package Architecture;
+
+import java.util.List;
+
+public class UnsignedBinaryNumber {
+    private List<Integer> BinaryRepresentation;
+    private int Length;
+    private int MaxValue;
+    private int MinValue;
+    private boolean Overflown;
+
+    public UnsignedBinaryNumber(int entryValue) {
+
+    }
+
+    public UnsignedBinaryNumber(int entryValue, int length) {
+
+    }
+
+    public UnsignedBinaryNumber(String number) {
+
+    }
+
+    public UnsignedBinaryNumber(String number, int length) {
+
+    }
+
+    public UnsignedBinaryNumber(SignedBinaryNumber signedBinaryNumber){
+
+    }
+
+    public UnsignedBinaryNumber(SignedBinaryNumber signedBinaryNumber, int length){
+
+    }
+
+    public UnsignedBinaryNumber(BasedInt signedBinaryNumber){
+
+    }
+
+    public UnsignedBinaryNumber(BasedInt signedBinaryNumber, int length){
+
+    }
+
+    private List<Integer> toBinNumber(){
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+
+    public UnsignedBinaryNumber Add(){
+
+    }
+
+    public UnsignedBinaryNumber Substract(){
+
+    }
+
+    public UnsignedBinaryNumber Multiply(){
+
+    }
+
+    public UnsignedBinaryNumber Divide(){
+
+    }
+
+    public UnsignedBinaryNumber ToOnesComplement(){
+
+    }
+
+    public UnsignedBinaryNumber ToTwosComplement(){
+
+    }
+
+    public SignedBinaryNumber ToOpposite(){
+
+    }
+
+    public UnsignedBinaryNumber ToSigned(){
+
+    }
+
+    public static boolean CanFitInRepresentation(int value, int length){
+
+    }
+
+    public boolean CanFitInRepresentation(){
+
+    }
+
+    public static boolean CanFitInRepresentation(){
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+    }
+}
