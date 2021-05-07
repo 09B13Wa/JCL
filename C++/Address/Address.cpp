d7ff7607c8bf99ca5fd0d6291613b8b19906a18d
jaschamerle <by8vbcs97b@privaterelay.appleid.com>
2021-05-07 12:05:56 +0200

    adding start of JCL for C++

---
+++ C++/Address/Address.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jasch on 5/7/2021.
+//
+
+#include "Address.h"
---
+++ C++/Address/Address.h
@@ -0,0 +1,14 @@
+//
+// Created by jasch on 5/7/2021.
+//
+
+#ifndef C___ADDRESS_H
+#define C___ADDRESS_H
+
+
+class Address {
+
+};
+
+
+#endif //C___ADDRESS_H
