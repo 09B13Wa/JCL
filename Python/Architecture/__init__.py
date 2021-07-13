45521d5b27ad91cbf30e0e0e9a9423efa4de7cad
jaschamerle <by8vbcs97b@privaterelay.appleid.com>
2021-07-13 19:01:48 +0200

    Webhook test

--- Python/Architecture/__init__.py
+++ Python/Architecture/__init__.py
@@ -6,6 +6,7 @@ from threading import Thread
 
 boucle_allume: bool = True
 
+
 def example(limite, enieme):
     global boucle
     boucle.rai
@@ -13,6 +14,24 @@ def example(limite, enieme):
     while i < limite:
         print(str(i) + " iteration de la boucle n°" + enieme)
         i += 1
+
+
+import threading
+
+
+class StoppableThread(threading.Thread):
+    """Thread class with a stop() method. The thread itself has to check
+    regularly for the stopped() condition."""
+
+    def __init__(self,  *args, **kwargs):
+        super(StoppableThread, self).__init__(*args, **kwargs)
+        self._stop_event = threading.Event()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
 
 
 boucle = Thread(target=example, args=[20, "1"])
